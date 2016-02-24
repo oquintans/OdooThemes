e4af898a7948869deb3540e6be5508aa4f3dda39
oquintansocampo <oquintansocampo@danielcastelao.org>
2016-02-24 12:01:45 +0100

    snippets in process

--- theme_odoo/__openerp__.py
+++ theme_odoo/__openerp__.py
@@ -1,9 +1,9 @@
          {
   'name':'Tema Odoo',
-  'description': 'A description for your theme.',
+  'description': '1.6',
   'version':'1.0',
   'author':'oquintans',
-  'data': ['views/layout.xml','views/pages.xml','views/assets.xml'],
+  'data': ['views/layout.xml','views/pages.xml','views/assets.xml','views/snippets.xml'],
   'category': 'Theme/Creative',
   'depends': ['website']
 }
